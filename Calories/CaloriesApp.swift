99c3519f83ef0fc5378208fbbe435012b704a45b
William Chrisandy <william_chrisandy@yahoo.com>
2025-05-07 04:25:38 +0700

    Initial Commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by William Chrisandy on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
